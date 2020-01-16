0d2f2b84e73054442a854d56021f10da8effaf0a
jack19990504 <jack19990504@gmail.com>
2020-01-16 12:06:59 +0800

    test

--- src/main/java/com/activity/entity/Member.java
+++ src/main/java/com/activity/entity/Member.java
@@ -4,31 +4,91 @@ import java.sql.Date;
 
 public class Member {
 
-	private String email;
+	private String memberEmail;
 	
-	private String password;
+	private String memberPassword;
 	
-	private String name;
+	private String memberName;
 	
-	private String gender;
+	private String memberGender;
 	
-	private Date birthday;
+	private Date memberBirthday;
 	
-	private String tel;
+	private String memberTel;
 	
-	private String phone;
+	private String memberPhone;
 	
-	private String address;
-	
-	private String lineId;
+	private String memberAddress;
 	
 	
 	private String memberLineId;
 	
 	private Integer type;
 
-	
-	
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+
+	public String getMemberPassword() {
+		return memberPassword;
+	}
+
+	public void setMemberPassword(String memberPassword) {
+		this.memberPassword = memberPassword;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public String getMemberGender() {
+		return memberGender;
+	}
+
+	public void setMemberGender(String memberGender) {
+		this.memberGender = memberGender;
+	}
+
+	public Date getMemberBirthday() {
+		return memberBirthday;
+	}
+
+	public void setMemberBirthday(Date memberBirthday) {
+		this.memberBirthday = memberBirthday;
+	}
+
+	public String getMemberTel() {
+		return memberTel;
+	}
+
+	public void setMemberTel(String memberTel) {
+		this.memberTel = memberTel;
+	}
+
+	public String getMemberPhone() {
+		return memberPhone;
+	}
+
+	public void setMemberPhone(String memberPhone) {
+		this.memberPhone = memberPhone;
+	}
+
+	public String getMemberAddress() {
+		return memberAddress;
+	}
+
+	public void setMemberAddress(String memberAddress) {
+		this.memberAddress = memberAddress;
+	}
+
 	public String getMemberLineId() {
 		return memberLineId;
 	}
@@ -44,80 +104,7 @@ public class Member {
 	public void setType(Integer type) {
 		this.type = type;
 	}
-
-	public String getLineId() {
-		return lineId;
-	}
-
-	public void setLineId(String lineId) {
-		lineId = lineId;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public Date getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
-
-	public String getTel() {
-		return tel;
-	}
-
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
 	
-
-
+	
 	
 }
