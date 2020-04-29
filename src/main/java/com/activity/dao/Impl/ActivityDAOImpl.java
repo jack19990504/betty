7092e3bab4237599f6bd1f3a120d10f32a609dff
ahsuanchen <ahsuan1027@gmail.com>
2020-04-29 21:57:15 +0800

    537

--- src/main/java/com/activity/dao/Impl/ActivityDAOImpl.java
+++ src/main/java/com/activity/dao/Impl/ActivityDAOImpl.java
@@ -466,10 +466,10 @@ public class ActivityDAOImpl implements ActivityDAO {
 				activity.setEndSignUpDate(rs.getTimestamp("endSignUpDate"));
 				
 				//program control
-				activity.setActivityStartDateString(rs.getTimestamp("activityStartDate") != null ? rs.getTimestamp("activityStartDate").toString() : "");
-				activity.setActivityEndDateString(rs.getTimestamp("activityEndDate") != null ? rs.getTimestamp("activityEndDate").toString() : "");
-				activity.setStartSignUpDateString(rs.getTimestamp("startSignUpDate") != null ? rs.getTimestamp("startSignUpDate").toString() : "");
-				activity.setEndSignUpDateString(rs.getTimestamp("endSignUpDate") != null ? rs.getTimestamp("endSignUpDate").toString() : "");
+				activity.setActivityStartDateString(rs.getTimestamp("activityStartDate") != null ? rs.getTimestamp("activityStartDate").toString().substring(0,16) : "");
+				activity.setActivityEndDateString(rs.getTimestamp("activityEndDate") != null ? rs.getTimestamp("activityEndDate").toString().substring(0,16) : "");
+				activity.setStartSignUpDateString(rs.getTimestamp("startSignUpDate") != null ? rs.getTimestamp("startSignUpDate").toString().substring(0,16) : "");
+				activity.setEndSignUpDateString(rs.getTimestamp("endSignUpDate") != null ? rs.getTimestamp("endSignUpDate").toString().substring(0,16) : "");
 				
 				
 				activity.setActivityMeal(rs.getInt("activityMeal"));
